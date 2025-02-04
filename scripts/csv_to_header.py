3a6d5660fa99681a70a8321d741c59f497213fad
keltonhalbert <kthalbert@wisc.edu>
2025-02-04 10:43:21 -0500

    moving Meteogram struct elsewhere now that testing is done

--- scripts/csv_to_header.py
+++ scripts/csv_to_header.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import numpy as np
 
+
 def main():
 
-    df = pd.read_csv("../data/2024-03-CHEY-firewx.csv", parse_dates=["DateTime"])
+    df = pd.read_csv("../data/2024-03-CHEY-firewx.csv",
+                     parse_dates=["DateTime"])
     df["DateTime"] = df["DateTime"].astype('int').div(10**9)
     df["WMAX"] = np.around(df["WMAX"], decimals=2)
 
     header_start_str = "#ifndef NFDRS4_TEST_DATA\n#define NFDRS4_TEST_DATA\n"
     header_end_str = "\n#endif\n"
 
-    struct_start_str = "struct Meteogram {\n"
-    struct_end_str = "};\n"
     size_str = f"static constexpr int N = {df['RELH'].shape[0]};\n"
     time_str = "static constexpr double timestamp[] = {"
     relh_str = "static constexpr double relh[] = {"
@@ -22,7 +22,6 @@ def main():
     rain_str = "static constexpr double rain[] = {"
     pres_str = "static constexpr double pres[] = {"
     srad_str = "static constexpr double srad[] = {"
-    firewx_str = "int firewx_cat[N] = {"
 
     time_str += ','.join(f"{val}" for val in df['DateTime'])
     relh_str += ','.join(f"{val}" for val in df['RELH'])
@@ -33,7 +32,6 @@ def main():
     rain_str += ','.join(f"{val}" for val in df['RAIN'])
     pres_str += ','.join(f"{val}" for val in df['PRES'])
     srad_str += ','.join(f"{val}" for val in df['SRAD'])
-    firewx_str += ','.join(f"{val}" for val in np.zeros(df["RELH"].shape[0], dtype=int))
 
     time_str += "};\n"
     relh_str += "};\n"
@@ -44,11 +42,9 @@ def main():
     rain_str += "};\n"
     pres_str += "};\n"
     srad_str += "};\n"
-    firewx_str += "};\n"
 
     with open("../include/NFDRSGUI/data.h", 'w') as outfile:
         outfile.write(header_start_str)
-        outfile.write(struct_start_str)
         outfile.write(size_str)
         outfile.write(time_str)
         outfile.write(relh_str)
@@ -59,10 +55,7 @@ def main():
         outfile.write(rain_str)
         outfile.write(pres_str)
         outfile.write(srad_str)
-        outfile.write(firewx_str)
-        outfile.write(struct_end_str)
         outfile.write(header_end_str)
-
 
 
 if __name__ == "__main__":
