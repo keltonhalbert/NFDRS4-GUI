dbd436ae83a858167bd5ba224faeb763dfc09a0d
keltonhalbert <kthalbert@wisc.edu>
2025-02-05 15:56:07 -0500

    Added settings (including defaults) for the DeadFuelModels

--- include/NFDRSGUI/ModelRunners.h
+++ include/NFDRSGUI/ModelRunners.h
@@ -8,6 +8,24 @@
 #include <memory>
 #include <thread>
 
+struct DeadFuelSettings {
+    double adsorption_rate;
+    double desorption_rate = 0.06;
+    double planar_heat_transfer_rate;
+    double max_local_moisture = 0.6;
+    double stick_density = 0.4;
+    double stick_length = 41.0;
+    int diffusivity_steps;
+    int moisture_steps;
+    int stick_nodes = 11;
+    int random_seed = 0;
+    bool use_derived_adsorption_rate = true;
+    bool use_derived_diffusivity_steps = true;
+    bool use_derived_moisture_steps = true;
+    bool use_derived_planar_hear_transfer_rate = true;
+    bool use_derived_stick_nodes = true;
+};
+
 struct DeadFuelModelRunner {
     std::unique_ptr<DeadFuelMoisture> model;
 
